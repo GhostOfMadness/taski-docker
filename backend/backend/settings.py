2a4d04f4e34140a1266e40fdfa99fabd3ce9b04f
Daria Ovechkina <GhostofMadnessNN@yandex.ru>
2023-06-15 18:21:29 +0300

    Set DEBUG to False

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '158.160.71.69',
+    'ghostofmadnessnn.hopto.org',
+]
 
 
 # Application definition
